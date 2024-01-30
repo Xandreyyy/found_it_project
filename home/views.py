669302f75f38e8b2e4de6451044af0caabd8cfd3
Xandreyyy <alejandroesteves2005@gmail.com>
2024-01-30 13:56:24 -0300

    migrating to django #3

--- home/views.py
+++ home/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+import requests
+
+url = "https://us1.locationiq.com/v1/search"
+
+data = {
+    'key': 'pk.9bac73b8821d501d511a29413e757052',
+    'q': 'Viamão, RS, Brazil',
+    'format': 'json'
+}
+
+response = requests.get(url, params = data)
+print(response.json())
 
 def home(request):
     return render(request, "home/index.html")
